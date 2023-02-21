21a03ea249b843524487bcd3883d52858f4bfeec
Jad229 <76543236+Jad229@users.noreply.github.com>
2023-02-21 17:43:38 -0500

    CHANGES: created navigation bar component

---
+++ Frontend/pretty-queen-vue/src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap/dist/css/bootstrap.css";
+import "./assets/main.css";
+
+createApp(App).mount("#app");
