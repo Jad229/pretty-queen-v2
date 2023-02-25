68c69290243a3de10c578a97ee840a2758120678
Jad229 <jad229@njit.edu>
2023-02-24 21:36:45 -0500

    FEATURE: created footer

--- Frontend/pretty-queen-vue/src/App.vue
+++ Frontend/pretty-queen-vue/src/App.vue
@@ -1,15 +1,20 @@
 <script>
 import NavigationBar from "./components/NavigationBar.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   components: {
     NavigationBar,
+    Footer,
   },
 };
 </script>
 
 <template>
-  <NavigationBar />
+  <body>
+    <NavigationBar />
+    <Footer />
+  </body>
 </template>
 
 <style scoped></style>
