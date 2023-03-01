18a9db171d587670151491812cb2b8c6f4b260bb
Jad229 <jad229@njit.edu>
2023-03-01 15:37:09 -0500

    FEATURE: added code to Service page component

---
+++ Frontend/pretty-queen-vue/src/components/ServicePage.vue
@@ -0,0 +1,19 @@
+<script>
+import NavigationBar from "./NavigationBar.vue";
+import Footer from "./Footer.vue";
+export default {
+  name: "ServicePage",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <body>
+    <NavigationBar />
+    <Footer />
+  </body>
+</template>
+
+<style scoped></style>
