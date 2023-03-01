26fdd21acf24fa4c3dde347359e112f9559a322a
Jad229 <76543236+Jad229@users.noreply.github.com>
2023-03-01 12:13:53 -0500

    FEATURE: created a vue router which routes the components

--- Frontend/pretty-queen-vue/src/main.js
+++ Frontend/pretty-queen-vue/src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from "./routes";
 
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/styles.css";
+
+app.use(router);
 
 createApp(App).mount("#app");
