69fe14190d2269fc59b685deafd88d2101dbca5e
Jad229 <jad229@njit.edu>
2023-03-01 18:26:44 -0500

    FEATURE: created Products page component

---
+++ Frontend/pretty-queen-vue/src/components/ProductsPage.vue
@@ -0,0 +1,23 @@
+<script>
+import NavigationBar from "./NavigationBar.vue";
+import Footer from "./Footer.vue";
+
+export default {
+  name: "ProductsPage",
+  components: {
+    NavigationBar,
+    Footer,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <NavigationBar />
+  <h1>ProductsPage</h1>
+  <Footer />
+</template>
+
+<style scoped></style>
